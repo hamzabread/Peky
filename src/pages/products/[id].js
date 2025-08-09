fb9adc8c5895a9482b107229d143cfae859b9c74
hamzabread <hamzaval2000@gmail.com>
2025-08-09 18:31:41 +0500

    new test

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,20 +1,20 @@
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-  const res = await fetch(`http://localhost:5000/products/${id}`);
-  const product = await res.json();
+// export async function getServerSideProps(context) {
+//   const { id } = context.params;
+//   const res = await fetch(`http://localhost:5000/products/${id}`);
+//   const product = await res.json();
 
-  return {
-    props: { product },
-  };
-}
+//   return {
+//     props: { product },
+//   };
+// }
 
-export default function ProductPage({ product }) {
-  return (
-    <div>
-      <h1>{product.title}</h1>
-      <img src={product.image} alt={product.title} />
-      <p>{product.description}</p>
-      <p>{product.price}</p>
-    </div>
-  );
-}
+// export default function ProductPage({ product }) {
+//   return (
+//     <div>
+//       <h1>{product.title}</h1>
+//       <img src={product.image} alt={product.title} />
+//       <p>{product.description}</p>
+//       <p>{product.price}</p>
+//     </div>
+//   );
+// }
