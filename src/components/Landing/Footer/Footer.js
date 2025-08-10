9eefd0652263a10f8ae652643aaa1a4e210d55ba
hamzabread <hamzaval2000@gmail.com>
2025-08-11 02:02:04 +0500

    final-website

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -5,14 +5,15 @@ const Footer = () => {
     <section className='bg-black text-[#FFF] p-[50px] py-[60px]'>
 
         <div className="flex items-center">
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col w-full items-center lg:items-start">
                 <img src="/assets/header/pekyicon.jpeg" alt="" className='h-[50px] !mb-[50px]' />
                 <h3 className='text-[30px] !mb-[15px]'>Contacts</h3>
                 <div className='flex items-center gap-[10px]'>
                     <p className='text-[20px] pl-[5px]'>+92 12345678</p>
                 </div>
                 <div className="flex items-center gap-[10px] !mt-[5px]">
-                    <p className='text-[20px] pl-[5px]'>peky@gmail.com</p>
+                    <p className='text-[20px] pl-[5px]'>
+pekypkcontact@gmail.com</p>
                 </div>
             </div>
         </div>
