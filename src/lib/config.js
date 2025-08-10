817f04c49ea4156700eb286411a64ee322c71784
hamzabread <hamzaval2000@gmail.com>
2025-08-10 18:16:06 +0500

    vercelfinal

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 let API_URL;
 
 if (process.env.NODE_ENV === "development") {
-  API_URL = "http://localhost:5000"; // local Flask run
+  API_URL = "http://localhost:5000"; // local Flask run inshallah
 } else {
   API_URL = process.env.NEXT_PUBLIC_API_URL; // production on Vercel
 }
