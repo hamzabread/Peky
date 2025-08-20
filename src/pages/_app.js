10ab1dc4a6e12ce6b97b20f25fc4b2fc5185fabf
hamzabread <hamzaval2000@gmail.com>
2025-08-21 01:47:58 +0500

    favicon-added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,20 @@
-
 import "@/styles/app.css";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
-import { Urbanist, DM_Sans } from 'next/font/google';
+import { Urbanist, DM_Sans } from "next/font/google";
 
 const dmSans = DM_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-dm-sans',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-dm-sans",
+  display: "swap",
 });
 
 const urbanist = Urbanist({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
-  variable: '--font-urbanist',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-urbanist",
+  display: "swap",
 });
 
 export default function App({ Component, pageProps }) {
@@ -26,8 +25,27 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
-        <meta name="description" content="Peky is a company that sells premium aluminium foil"></meta>
+        <meta
+          name="description"
+          content="Peky is a company that sells premium aluminium foil"
+        ></meta>
         <title>peky</title>
+              {/* Standard favicon */}
+        <link rel="icon" href="/favicon/favicon.ico" />
+
+        {/* PNG icons */}
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/web-app-manifest-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/web-app-manifest-512x512.png" />
+
+        {/* Apple touch icon */}
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+
+        {/* SVG icon (modern browsers) */}
+        <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
+
+        {/* Manifest */}
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <ToastContainer />
       <Component {...pageProps} />
