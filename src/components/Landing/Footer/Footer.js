1d8dba35ff15c70ff980344fe8c6e843055cbe94
hamzabread <hamzaval2000@gmail.com>
2025-08-21 22:36:59 +0500

    fixes

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
           <h3 className="text-[30px] !mb-[15px]">Contacts</h3>
           <div className="flex flex-col items-center gap-[10px] sm:flex-row">
             <p className="text-[20px] pl-[5px]">+92 3159982783</p>
-            <p className="text-[20px] pl-[5px]">+92 3369992787</p>
+            <p className="text-[20px] pl-[5px]">+92 3369982787</p>
           </div>
           <div className="flex items-center gap-[10px] !mt-[5px]">
             <p className="text-[20px] pl-[5px]">pekypk25@gmail.com</p>
